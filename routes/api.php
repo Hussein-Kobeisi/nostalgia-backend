ab78f03d45f80fff7ac046b649de8344881ed3b0
HKob25 <husseinakobeisi@gmail.com>
2025-07-21 14:30:14 +0300

    Fixed conflicts

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CapsuleController;
+use App\Http\Controllers\CapsuleMediaController;
+use App\Http\Controllers\UserController;
 
 Route::group(['middleware' => 'auth:api'], function(){
     
